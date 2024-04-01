6ef329bf9fb6f839c5053fdde36a6c76778e4f69
rohansharma-developer <rohansharma.developer@skiff.com>
2024-04-01 13:21:24 +0530

    added functions

---
+++ functions.py
@@ -0,0 +1,16 @@
+def greet(n):
+    prime = True
+    if n==0 or n==1:
+        print("Neither prime nor composite")
+    elif n==2:
+        print("Prime Number")
+    else:
+        for num in range(2, int(n/2)):
+            if n%num==0:
+                print("Composite Number")
+                prime=False
+                break
+        if prime:
+            print("Prime Number")
+number = int(input("Enter number to check: "))
+greet(number)
